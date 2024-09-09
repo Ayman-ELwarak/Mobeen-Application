d9b4508b54272ebda21130186f2a44244f859bc9
Eman  Elharery <106907391+Eman-Elharery@users.noreply.github.com>
2024-09-09 03:40:24 +0300

    video model

---
+++ lib/models/video_model.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+class Video {
+  final String title;
+  final String assetPath;
+
+  Video({
+    required this.title,
+    required this.assetPath,
+  });
+}
